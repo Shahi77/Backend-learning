d0d308a55ed8de760ef6547300c058fd3e4b568d
Shahi77 <shahiinfinity02@gmail.com>
2023-10-23 11:03:57 +0530

    Express routes

---
+++ Express/server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 3000;
+
+const friends = [
+    {
+        id:0,
+        name:'Issac',
+    },
+    {
+        id:1,
+        name:'Einstein',
+    },
+    
+];
+app.get('/friends',(req,res)=>{
+  res.json(friends);
+})
+app.get('/friends/:friendId',(req,res)=>{
+    const friendId = req.params.friendId;
+    const friend = friends[friendId];
+    if(friend){
+        res.status(200).json(friend);
+    }
+    else{
+        res.status(404).json({
+            error:"Friend does not exist"
+        });
+    }
+})
+
+app.get('/messages',(req,res)=>{
+  res.send('<ul><li>Hello Otis</li></ul>');
+})
+
+app.post('/messages',(req,res)=>{
+    console.log('Updating messages...')
+})
+
+
+app.listen(PORT,() => {
+    console.log(`Listening on ${PORT}...`)
+});
