1e50116dbb446a3a22a5ebcbb2d61f5b147374d5
Shahi77 <shahiinfinity02@gmail.com>
2023-10-28 19:01:47 +0530

    adding performance example

---
+++ performance example/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cluster = require("cluster");
+const app = express();
+
+function delay(duration) {
+  const startTime = Date.now();
+  while (Date.now() - startTime < Duration) {
+    //event loop is blocked
+  }
+}
+app.get("/", (req, res) => {
+  res.send(`Performance example ${process.pid}`);
+});
+
+app.get("/timer", (req, res) => {
+  delay(9000);
+  res.send(`Ding Ding Ding! ${process.pid}`);
+});
+
+console.log("Running server.js...");
+if (cluster.isMaster) {
+  console.log("Master has been started!");
+  cluster.fork();
+  cluster.fork();
+} else {
+  console.log("Worker process started");
+  app.listen(3000);
+}
